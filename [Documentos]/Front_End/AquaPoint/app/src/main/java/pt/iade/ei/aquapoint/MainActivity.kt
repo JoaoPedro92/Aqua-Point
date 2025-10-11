54f0c26f311ef1368d560202414abc1e329cafb4
Joao <rua.joao92@gmail.com>
2025-10-11 21:06:53 +0100

    Primeiro commit do front end
    Este commit inclui a página inicial, login e registo já desenvolvidas.
    
    Serve de base para todos os outros branches também.
    
    Versão estável e testada.

---
+++ [Documentos]/Front_End/AquaPoint/app/src/main/java/pt/iade/ei/aquapoint/MainActivity.kt
@@ -0,0 +1,41 @@
+package pt.iade.ei.aquapoint
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pt.iade.ei.aquapoint.ui.theme.AquaPointTheme
+import pt.iade.ei.aquapoint.ui.components.CreateHomePage
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AquaPointTheme {
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainActivityPreview() {
+    AquaPointTheme {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text("StandX")
+
+            CreateHomePage()
+        }
+    }
+}
---
+++ [Documentos]/Front_End/AquaPoint/app/src/main/java/pt/iade/ei/aquapoint/ui/components/CreateHomePage.kt
@@ -0,0 +1,327 @@
+package pt.iade.ei.aquapoint.ui.components
+
+import android.graphics.fonts.FontFamily
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import pt.iade.ei.aquapoint.R
+import pt.iade.ei.aquapoint.ui.theme.AquaPointTheme
+import pt.iade.ei.aquapoint.ui.theme.RobotoFont
+import pt.iade.ei.aquapoint.ui.theme.AquaGreen
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import pt.iade.ei.aquapoint.ui.theme.ComfortaaFont
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.material3.IconButton
+
+@Composable
+fun CreateHomePage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 0.dp, vertical = 5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Image(
+            painter = painterResource(R.drawable.aqua_point_logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .height(100.dp)
+                .width(150.dp)
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.8f), // ocupa 70% da altura
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.home_page_image),
+                contentDescription = "Home Page Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop // ajusta a imagem
+            )
+        }
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Row (
+            modifier = Modifier
+        ){
+            Button(
+                onClick = {  },
+                shape = RoundedCornerShape(16.dp),
+                border = BorderStroke(2.dp, AquaGreen),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(0.5f)
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.log_in),
+                    fontFamily = RobotoFont,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = AquaGreen
+                )
+            }
+
+            Button(
+                onClick = {  },
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = AquaGreen,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(0.95f)
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.register),
+                    fontFamily = RobotoFont,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateRegisterPage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = "return",
+                tint = Color.Gray
+            )
+        }
+
+        Spacer(modifier = Modifier.height(80.dp))
+
+        Text(
+            text = stringResource(R.string.register_low),
+            fontFamily = ComfortaaFont,
+            fontWeight = FontWeight.Bold,
+            fontSize = 36.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(top = 16.dp, bottom = 32.dp)
+        )
+
+        Spacer(modifier = Modifier.height(70.dp))
+
+        OutlinedTextField(
+            value = "",
+            shape = RoundedCornerShape(16.dp),
+            onValueChange = {  },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = AquaGreen,
+                unfocusedBorderColor = AquaGreen,
+                focusedLabelColor = AquaGreen,
+                unfocusedLabelColor = AquaGreen,
+            ),
+            label = { Text(stringResource(R.string.mail_example)) },
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .padding(vertical = 8.dp),
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = "",
+            shape = RoundedCornerShape(16.dp),
+            onValueChange = {  },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = AquaGreen,
+                unfocusedBorderColor = AquaGreen,
+                focusedLabelColor = AquaGreen,
+                unfocusedLabelColor = AquaGreen,
+            ),
+            label = { Text(stringResource(R.string.password_input)) },
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .padding(vertical = 8.dp),
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(64.dp))
+
+        Button(
+            onClick = {  },
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = AquaGreen
+            ),
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .height(50.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.submit),
+                fontFamily = RobotoFont,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun CreateLoginPage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = "return",
+                tint = Color.Gray
+            )
+        }
+
+        Spacer(modifier = Modifier.height(80.dp))
+
+        Text(
+            text = stringResource(R.string.login_low),
+            fontFamily = ComfortaaFont,
+            fontWeight = FontWeight.Bold,
+            fontSize = 36.sp,
+            color = Color.Black,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(top = 16.dp, bottom = 32.dp)
+        )
+
+        Spacer(modifier = Modifier.height(70.dp))
+
+        OutlinedTextField(
+            value = "",
+            shape = RoundedCornerShape(16.dp),
+            onValueChange = {  },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = AquaGreen,
+                unfocusedBorderColor = AquaGreen,
+                focusedLabelColor = AquaGreen,
+                unfocusedLabelColor = AquaGreen,
+            ),
+            label = { Text(stringResource(R.string.mail_example)) },
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .padding(vertical = 8.dp),
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = "",
+            shape = RoundedCornerShape(16.dp),
+            onValueChange = {  },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = AquaGreen,
+                unfocusedBorderColor = AquaGreen,
+                focusedLabelColor = AquaGreen,
+                unfocusedLabelColor = AquaGreen,
+            ),
+            label = { Text(stringResource(R.string.password_input)) },
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .padding(vertical = 8.dp),
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(64.dp))
+
+        Button(
+            onClick = {  },
+            shape = RoundedCornerShape(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = AquaGreen
+            ),
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .height(50.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.log_in),
+                fontFamily = RobotoFont,
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                color = Color.White
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ListItemPreview() {
+    AquaPointTheme {
+        CreateLoginPage()
+    }
+}
