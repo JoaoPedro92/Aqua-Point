3c8f85c8cb70cfc16e29e52edbfc946980fc9712
Joao <48494671+JoaoPedro92@users.noreply.github.com>
2025-10-23 16:08:28 +0100

    Inicio do desenvolvimento do back-end da app
    Criação base dos ficheiros rest

---
+++ Back_End/AquaPoint/src/main/java/pt/iade/AquaPoint/AquaPointApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.AquaPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaPointApplication.class, args);
+	}
+
+}
